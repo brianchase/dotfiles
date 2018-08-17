da64ac4bf4f5f774e6836c04cb780e17bec7307b
brianchase <brianchase@mail.com>
2018-08-17 11:37:51 -0500

    change vpn script name

--- .bin/vpn-xsetroot.sh
+++ .bin/vpn-xsetroot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# vpn-client.sh: https://github.com/brianchase/vpn-client
+# client-openvpn.sh: https://github.com/brianchase/client-openvpn
 # status-dwm.sh: https://github.com/brianchase/dotfiles/.bin
 
 if [ -x "$(command -v vpn-client.sh)" ] && [ -x "$(command -v status-dwm.sh)" ]; then
-  vpn-client.sh "$1" "$2" && sleep 2 && status-dwm.sh
+  client-openvpn.sh "$1" "$2" && sleep 2 && status-dwm.sh
 fi
