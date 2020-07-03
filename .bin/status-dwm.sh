a75d5209bf267a5f6e469bcf21bcde670580d3a4
brianchase <brianmichaelchase@gmail.com>
2020-07-03 08:50:16 -0500

    better structure

--- .bin/status-dwm.sh
+++ .bin/status-dwm.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 Date="$(date +"%-l:%M %P %a %b %-d %Y")"
-External_IP="$(dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null)"
-Internal_IP="$(hostname -i | awk '{print $1}' | grep 192)"
-ISP_IP="$(hostname -i | awk '{print $2}')"
-if [ "$Internal_IP" ]; then
-  if [ "$External_IP" ]; then
-    xsetroot -name " $Internal_IP · $External_IP · $Date"
-  elif [ "$ISP_IP" ]; then
-    xsetroot -name " $Internal_IP · $ISP_IP · $Date"
-  else
-    xsetroot -name " $Internal_IP · $Date"
-  fi
-else
+LAN_IP="$(hostname -i | awk '{print $1}' | grep 192)"
+
+if [ -z "$LAN_IP" ]; then
   xsetroot -name " $Date"
+elif ! ping -q -w1 -c1 8.8.8.8 &>/dev/null; then
+  xsetroot -name " $LAN_IP · $Date"
+else
+  External_IP="$(dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null)"
+  ISP_IP="$(hostname -i | awk '{print $2}')"
+  if [ "$External_IP" ]; then
+    xsetroot -name " $LAN_IP · $External_IP · $Date"
+  else
+    xsetroot -name " $LAN_IP · $ISP_IP · $Date"
+  fi
 fi
