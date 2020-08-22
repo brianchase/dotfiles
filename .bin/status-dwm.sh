c0ef5af666d73f5e404e16f3c4518dabb656d4ea
brianchase <brianmichaelchase@gmail.com>
2020-08-22 10:33:30 -0500

    no empty strings

--- .bin/status-dwm.sh
+++ .bin/status-dwm.sh
@@ -2,17 +2,13 @@
 
 Date="$(date +"%-l:%M %P %a %b %-d %Y")"
 LAN_IP="$(hostname -i | awk '{print $1}' | grep 192)"
-
 if [ -z "$LAN_IP" ]; then
   xsetroot -name " $Date"
-elif ! ping -q -w1 -c1 8.8.8.8 &>/dev/null; then
-  xsetroot -name " $LAN_IP · $Date"
 else
   External_IP="$(dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null)"
-  ISP_IP="$(hostname -i | awk '{print $2}')"
-  if [ "$External_IP" ]; then
-    xsetroot -name " $LAN_IP · $External_IP · $Date"
+  if [ -z "$External_IP" ]; then
+    xsetroot -name " $LAN_IP · $Date"
   else
-    xsetroot -name " $LAN_IP · $ISP_IP · $Date"
+    xsetroot -name " $LAN_IP · $External_IP · $Date"
   fi
 fi
